f2f341ed538b093a41a132cabbf2ee04b5d3e975
carina-kauf <ckauf@mit.edu>
2022-03-03 12:26:50 -0500

    mv baseline scores

--- mv_model_scores.py
+++ mv_model_scores.py
@@ -4,6 +4,7 @@ Move files from model score creation directory (Event_Knowledge_Model_Comparison
 
 import glob, os
 import shutil
+import re
 
 os.makedirs('model_scores', exist_ok=True)
 
@@ -13,10 +14,18 @@ name2exp = {
     'dtfit': 'DTFit'
 }
 
+variations = {
+    'ev1': re.compile('ev1[\.\_]'),
+    'dtfit': re.compile('dtfit\.|DTFit\_'),
+    'new-EventsAdapt': re.compile('(new-|newsentences_)EventsAdapt')
+}
+
 for exp in ['ev1', 'new-EventsAdapt', 'dtfit']:
     score_dir = f'model_scores/{name2exp[exp]}'
     os.makedirs(score_dir, exist_ok=True)
-    files = glob.glob(f'Event_Knowledge_Model_Comparison/results/**/{exp}.*', recursive=True)
+    pat = variations[exp]
+
+    files = [f for f in glob.glob(f'Event_Knowledge_Model_Comparison/results/**/*.txt', recursive=True) if re.search(pat, f)]
     print(len(files))
     for file in files:
         shutil.copy(file, score_dir)
