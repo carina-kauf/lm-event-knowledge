8b7a9fc72038af1a9e29aa7b8f4c7a619eaf5e6a
Jingyuan She <jshe@openmind7.mit.edu>
2022-04-12 10:18:20 -0400

    eventsrev py and sh files

---
+++ probing/roberta_EventsRev_parallel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                         
+#SBATCH -t 05:00:00
+#SBATCH -n 1                      
+#SBATCH --gres=gpu:titan-x:1
+#SBATCH -p evlab     
+#SBATCH --array=1-24       
+python roberta_DTFit_parallel.py $SLURM_ARRAY_TASK_ID
