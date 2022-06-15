45c8fc83001c471e5e8b4f36e2f4766c812abdbe
Jingyuan She <79201969+selena-she@users.noreply.github.com>
2022-06-15 14:07:17 -0400

    Delete probing/old directory

--- probing/old/pickle_automatic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -t 01:30:00
-#SBATCH -n 1
-#SBATCH --gres=gpu:titan-x:1
-#SBATCH --mem=20G
-#SBATCH -p evlab     
-module load openmind/anaconda/3-2019.10
-python pickle_newsplit_automatic.py 48 'active-active' 'normal-AAR' 'EventsAdapt' 'gpt2-xl'
--- probing/old/roberta_parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash                         
-#SBATCH -t 05:00:00
-#SBATCH -n 1                      
-#SBATCH --gres=gpu:titan-x:1
-#SBATCH -p evlab     
-#SBATCH --array=1-25
-module load openmind/anaconda/3-2019.10       
-python roberta_parallel.py $SLURM_ARRAY_TASK_ID '/om2/user/jshe/lm-event-knowledge/analyses_clean/clean_data/clean_EventsAdapt_SentenceSet.csv'
-
